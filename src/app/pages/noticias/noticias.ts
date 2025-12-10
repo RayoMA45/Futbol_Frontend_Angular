47e535317e066bbdd39d51b1d8cbef4382891c1f
Marco Rayo <rayobm5@gmail.com>
2025-12-09 19:24:22 -0600

    [UPDATE] vista de noticias, cards

--- src/app/pages/noticias/noticias.ts
+++ src/app/pages/noticias/noticias.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Menu } from '../../shared/menu/menu';
 import { Carousel } from "./components/carousel/carousel";
+import { Card } from '../../shared/card/card';
 
 @Component({
   selector: 'app-noticias',
-  imports: [Menu, Carousel],
+  imports: [Menu, Carousel, Card],
   templateUrl: './noticias.html',
   styleUrl: './noticias.css',
 })
